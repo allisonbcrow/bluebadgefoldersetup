ab81d205e1c02fbd589fc764d0535de478a3320c
allisonbcrow <83992047+allisonbcrow@users.noreply.github.com>
2021-08-10 12:47:07 -0400

    upload on 8/10

---
+++ controllers/logcontroller.js
@@ -0,0 +1,62 @@
+let express = require('express');
+let router = express.Router();
+let validateSession = require('../middleware/validate-session');
+const Log = require('../db').import('../models/log');
+
+router.get('/practice', validateSession, function(req,res)
+{
+    res.send("Hey!! This is a practice route!")
+});
+
+router.post('/log', validateSession, (req, res) => {
+    const logEntry = {
+        description: req.body.log.description,
+        definition: req.body.log.definition,
+        result: req.body.log.result,
+        owner_id: req.user.id
+    }
+    Log.create(logEntry)
+    .then(log => res.status(200).json(log))
+    .catch(err => res.status(500).json({ error: err }))
+});
+
+router.get('/log', (req, res) => {
+    Log.findAll()
+    .then(logs => res.status(200).json(logs))
+    .catch(err => res.status(500).json({ error: err }))
+});
+
+router.get("/log/:id", validateSession, (req, res) => {
+   let userid = req.user.id
+    Log.findAll({
+        where: {  owner_id: userid }
+    })
+    .then(logs => res.status(200).json(logs))
+    .catch(err => res.status(500).json({ error:err }))
+});
+
+
+router.put("/log/:id", validateSession, function (req, res) {
+    const updateLogEntry = {
+        description: req.body.log.description,
+        definition: req.body.log.definition,
+        result: req.body.log.result,
+        owner_id: req.user.id
+    };
+
+    const query = { where: {id: req.params.id, owner_id: req.user.id } };
+
+    Log.update(updateLogEntry, query)
+    .then((logs) => res.status(200).json(logs))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+router.delete("/log/:id", validateSession, function (req,res) {
+    const query = { where: {id: req.params.id, owner_id: req.user.id } };
+
+    Log.destroy(query)
+    .then(() => res.status(200).json({message: "Log Entry Removed" }))
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+module.exports = router;
