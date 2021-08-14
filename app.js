d500f95e5ac786bae3ea029fc4d56de0a85d18df
Allison Crow <83992047+allisonbcrow@users.noreply.github.com>
2021-08-14 19:04:05 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config;
+let express = require('express');
+let app = express();
+const sequelize = require('./db');
+
+let log = require('./controllers/logcontroller');
+let user = require('./controllers/usercontroller');
+
+sequelize.sync();
+// sequelize.sync({force: true})
+app.use(require('./middleware/headers'));
+
+app.use(express.json());
+
+app.use('/user', user);
+
+app.use(require('./middleware'));
+app.use('/log', log);
+
+app.listen(3000, function () {
+    console.log("App is listening on port 3000");
+})
+
